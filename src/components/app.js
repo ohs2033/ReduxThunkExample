d57af7509e1693abde3b8f7c147dafefc684823c
David Murray <David Murray>
2016-04-23 23:07:23 -1000

    Adding initially

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import { Component } from 'react';
+import CommentBox from './comment_box';
+import CommentList from './comment_list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <CommentBox />
+          <br/>
+          <CommentList />
+      </div>
+
     );
   }
 }
+
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,6 +1,7 @@
 import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
+// Use describe to group together similar tests
 describe('App' , () => {
   let component;
 
@@ -8,7 +9,13 @@ describe('App' , () => {
     component = renderComponent(App);
   });
 
-  it('renders something', () => {
-    expect(component).to.exist;
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
   });
+
+    it('shows a comment list', () => {
+       expect(component.find('.comment-list')).to.exist;
+    });
 });
+
+// npm run test:watch
