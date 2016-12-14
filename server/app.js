97d31ef3d05820fee539d520e6131b685f5460a8
hs.o <ohs@ab180.co>
2016-12-15 00:51:33 +0900

    server

--- server/app.js
+++ server/app.js
@@ -6,6 +6,12 @@ app.get('/', function (req, res) {
   res.sendFile('../index.html');
 });
 
+app.get('/data', function (req, res) {
+  res.send(JSON.stringify({
+    resource: 30000
+  }))
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
